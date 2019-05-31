dd5dd063a65ba289842839d7e91aac69bd784417
mundoe1999 <estebanmundo1999@gmail.com>
2019-05-31 14:31:58 -0400

    Fixed HTML and improved JS formatting

--- script.js
+++ script.js
@@ -1,133 +1,151 @@
-var table = document.getElementById("pixelArt");
-var row = 0;
-var col = 4;
-var currColor = 'red';
+const table = document.getElementById("pixelArt");
+let row = 0;
+let col = 7;
+let currColor = 'red';
 const colorSelector = document.getElementById('colors');
-var mouseDown = 0;
+let mouseDown = 0;
 console.log(colorSelector);
 
-//options[document.getElementById("colors").selectedIndex].value;
 
+//Functions relating to the table itself
+
+
+//Inserting Row Function
 function insertRow() {
-    row++;
-    currColor = '#FFFFF2';
+  //Modify row variable
+  row++;
+  currColor = '#FFFFF2';
 
-    //Update table
-    let newRow = table.insertRow(-1);
-    for (let i = 0; i < col; i++) {
-        let newCell = newRow.insertCell(i);
-        newCell.style.backgroundColor = "#FFFFF2";
+  //Update table
+  let newRow = table.insertRow(-1);
 
-        //Click color change
-        newCell.addEventListener('mousedown', function(e) {
-            this.style.backgroundColor = currColor;
-        });
+  for (let i = 0; i < col; i++) {
+    let newCell = newRow.insertCell(i);
+    newCell.style.backgroundColor = "#FFFFF2";
 
-        newCell.addEventListener('mouseover', function(e) {
-            if (mouseDown)
-                this.style.backgroundColor = currColor;
-        });
+    //Click color change
+    newCell.addEventListener('mousedown', function(e) {
+      this.style.backgroundColor = currColor;
+    });
+
+    newCell.addEventListener('mouseover', function(e) {
+      if (mouseDown)
+        this.style.backgroundColor = currColor;
+    });
 
 
-    }
+  }
 
-    currColor = colorSelector.options[colorSelector.selectedIndex].value;
+  currColor = colorSelector.options[colorSelector.selectedIndex].value;
 }
 
+//Removing row function
 function removeRow() {
-    row--;
-    if (row < 0) row = 0;
+  //Modify row variable
+  row--;
+  if (row < 0) row = 0;
 
-    //Update table
-    table.deleteRow(-1);
+  //Update table
+  table.deleteRow(-1);
 }
 
 function addColumn() {
-    col++;
-    currColor = '#FFFFF2';
+  col++;
+  currColor = '#FFFFF2';
 
-    //Update table
-    for (let i = 0; i < row; i++) {
-        let newCell = table.rows[i].insertCell(-1);
-        newCell.style.backgroundColor = "#FFFFF2";
+  //Update table
+  for (let i = 0; i < row; i++) {
+    let newCell = table.rows[i].insertCell(-1);
+    newCell.style.backgroundColor = "#FFFFF2";
 
-        //change color onClick
-        newCell.addEventListener('mousedown', function(e) {
-            this.style.backgroundColor = currColor;
-        });
+    //adding event listeners to the cells
+    newCell.addEventListener('mousedown', function(e) {
+      this.style.backgroundColor = currColor;
+    });
 
-        newCell.addEventListener('mouseover', function(e) {
-            if (mouseDown)
-                this.style.backgroundColor = currColor;
-        });
+    newCell.addEventListener('mouseover', function(e) {
+      if (mouseDown)
+        this.style.backgroundColor = currColor;
+    });
 
 
-    }
-    currColor = colorSelector.options[colorSelector.selectedIndex].value;
+  }
+  currColor = colorSelector.options[colorSelector.selectedIndex].value;
 }
 
 function removeColumn() {
 
-    //Modify column variable
-    col--;
-    if (col < 0) col = 0;
+  //Modify column variable
+  col--;
+  if (col < 0) col = 0;
 
-    for (let i = 0; i < row; i++) {
-        table.rows[i].deleteCell(-1);
-    }
+  //Update table
+  for (let i = 0; i < row; i++) {
+    table.rows[i].deleteCell(-1);
+  }
 
 
 }
 
 
-//Color Change function
-
+//Changes set color 
 colorSelector.addEventListener('change', function() {
-    currColor = colorSelector.options[colorSelector.selectedIndex].value;
-    console.log(colorSelector.options[colorSelector.selectedIndex].value);
+  currColor = colorSelector.options[colorSelector.selectedIndex].value;
+  console.log(colorSelector.options[colorSelector.selectedIndex].value);
 });
 
 
 
 //Color Fill Functions
 
+//Fill all uncolored pixels
 function changeAllUncolored() {
-    for (let i = 0; i < row; i++) {
-        for (let j = 0; j < col; j++) {
-            let currCell = table.rows[i].cells[j];
-            let cellColor = currCell.style.backgroundColor;
-            if (cellColor === "rgb(255, 255, 242)") {
-                console.log("pass");
-                currCell.style.backgroundColor = currColor;
-            }
+  for (let i = 0; i < row; i++) {
+    for (let j = 0; j < col; j++) {
+      //Obtaining color
+      let currCell = table.rows[i].cells[j];
+      let cellColor = currCell.style.backgroundColor;
 
-        }
+      //Check for default color and replace
+      if (cellColor === "rgb(255, 255, 242)") {
+        console.log("pass");
+        currCell.style.backgroundColor = currColor;
+      }
+
     }
+  }
 }
 
+//Fill ALL pixels
 function changeAll() {
-    for (let i = 0; i < row; i++) {
-        for (let j = 0; j < col; j++) {
-            let currCell = table.rows[i].cells[j];
-            let cellColor = currCell.style.backgroundColor;
-            currCell.style.backgroundColor = currColor;
+  for (let i = 0; i < row; i++) {
+    for (let j = 0; j < col; j++) {
+      //Change color
+      let currCell = table.rows[i].cells[j];
+      let cellColor = currCell.style.backgroundColor;
+      currCell.style.backgroundColor = currColor;
 
-        }
     }
+  }
 }
 
+//Set all pixels to the default color
 function clearAll() {
-    for (let i = 0; i < row; i++) {
-        for (let j = 0; j < col; j++) {
-            let currCell = table.rows[i].cells[j];
-            currCell.style.backgroundColor = "rgb(255,255,242)";
-        }
+  for (let i = 0; i < row; i++) {
+    for (let j = 0; j < col; j++) {
+      let currCell = table.rows[i].cells[j];
+      currCell.style.backgroundColor = "rgb(255,255,242)";
     }
+  }
 }
+
+
+// helper functions that help track down
+// if the mouse is being held down.
 
 document.body.onmousedown = function() {
-    mouseDown = 1;
+  mouseDown = 1;
 }
 document.body.onmouseup = function() {
-    mouseDown = 0;
+  mouseDown = 0;
 }
